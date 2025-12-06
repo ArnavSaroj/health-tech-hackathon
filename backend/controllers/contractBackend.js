4821614b355e56b92c76440ff47d4c9b5a23f850
ARNAVSAROJ <178799881+ArnavSaroj@users.noreply.github.com.>
2025-12-06 15:12:03 +0530

    new commit

--- backend/controllers/contractBackend.js
+++ backend/controllers/contractBackend.js
@@ -1,20 +1,25 @@
 import { ethers } from "ethers";
-import dotenv from "dotenv";
-dotenv.config();
 
-// Correct JSON import syntax for Node 22
-import abi from "../StakeFit.json" with { type: "json" };
-
+const CONTRACT_ABI = [
+    "function verifyDay(address _user, bool _isValid) external"
+];
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS.trim();
+
+
 
 const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
-const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, wallet);
+const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, wallet);
 
 export default async function updateBlockchainResult(userAddress, isSuccess) {
   try {
+    console.log("Backend signer address:", wallet.address);
     console.log("Calling verifyDay for", userAddress, isSuccess);
+  const network = await provider.getNetwork();
+console.log("Network:", network);
+const balance = await provider.getBalance(wallet.address);
+console.log("Signer balance (ETH):", ethers.formatEther(balance));;
 
     const tx = await contract.verifyDay(userAddress, isSuccess);
     await tx.wait();
@@ -23,6 +28,6 @@ export default async function updateBlockchainResult(userAddress, isSuccess) {
 
   } catch (err) {
     console.error(err);
-    return { ok: false, error: err.message };
+    return { ok: false, error: err };
   }
 }
--- backend/controllers/verify.js
+++ backend/controllers/verify.js
@@ -2,6 +2,10 @@ import updateBlockchainResult from "./contractBackend.js";
 
 const verifyDay= async (req, res) => {
   const { user, isSuccess } = req.body;
+  
+  if (!user) {
+    return res.status(400).json({ ok: false, error: "Missing 'user' address in body" });
+  }
 
   const result = await updateBlockchainResult(user, isSuccess);
 
